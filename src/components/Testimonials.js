0f783411c395ed270d1ba40e01cffd5fa5e942d6
Srinivasa Y R. <srinivasyr2000@gmail.com>
2025-01-31 13:55:01 +0530

    testimonial changes

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -11,7 +11,7 @@ const Testimonials = () => {
             image: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png",
         },
         {
-            content: "Second testimonial: I've been a casual user of Figma, but as soon as I saw and started playing with FlowBite, my mind was blown and became so productive.",
+            content: "Second testimonial: As someone who mainly designs in the browser, I've been a casual user of Figma.",
             author: "Darshan",
             work: "CTO at Microsoft",
             image: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
@@ -23,7 +23,7 @@ const Testimonials = () => {
             image: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
         },
         {
-            content: "Fourth testimonial: As soon as I saw and started playing with FlowBite, my mind was blown and became so productive.",
+            content: "Fourth testimonial: As someone who mainly designs in the browser, I've been a casual user of Figma.",
             author: "Bonnie Green",
             work: "CTO at Microsoft",
             image: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
@@ -53,28 +53,20 @@ const Testimonials = () => {
         }, 300);
     };
 
-    // Auto-scroll functionality
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         handleNext();
-    //     }, 3000); 
-    //     return () => clearInterval(interval);
-    // }, []);
-
     return (
-        <div className="my-10 text-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIS6hfIuViVT1CS4zuf-NIwaJCx5z3mKJa7A&s')] bg-cover bg-center">
-            <h2 className="text-2xl font-bold dark:text-white text-white pt-6">Testimonials</h2>
-            <p className="text-xl text-white mt-2">
+        <div className="my-10 text-center bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIS6hfIuViVT1CS4zuf-NIwaJCx5z3mKJa7A&s')] bg-cover bg-center py-16">
+            <h2 className="text-2xl font-bold dark:text-white text-white pt-6 mb-4">Testimonials</h2>
+            <p className="text-xl text-white mt-2 max-w-3xl mx-auto">
                 Explore the whole collection of open-source web components and elements built
                 with the utility classes from Tailwind.
             </p>
             
-            <div className="flex justify-center items-center w-full">
-                <div className="w-2/3 dark:bg-gray-800 p-6 h-60 flex flex-col justify-center items-center transition-opacity duration-300 ease-in-out"
+            <div className="flex justify-center items-center w-full mt-6">
+                <div className="w-full sm:w-2/3 lg:w-1/2 dark:bg-gray-800 p-6 h-auto flex flex-col justify-center items-center transition-opacity duration-300 ease-in-out"
                     style={{ opacity: fade ? 0 : 1 }}>
                     
                     {/* Testimonial Content */}
-                    <p className="text-lg text-white">{`"${testimonialsData[currentIndex].content}"`}</p> 
+                    <p className="text-lg text-white text-center mb-4">{`"${testimonialsData[currentIndex].content}"`}</p> 
                     
                     <div className="flex items-center justify-center mt-4">
                         <img
@@ -96,7 +88,7 @@ const Testimonials = () => {
                     </div>
 
                     {/* Navigation Arrows */}
-                    <div className="mt-4 flex gap-4">
+                    <div className="mt-4 flex gap-4 justify-center">
                         <button className="p-2 text-white" onClick={handlePrev}>
                             <FontAwesomeIcon icon={faArrowLeft} />
                         </button>
